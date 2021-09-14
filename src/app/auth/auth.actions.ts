7a818054cc692fd5336f75415ad167d069372a39
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-09-13 19:41:14 -0700

    update

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,11 +1,71 @@
 import {createAction, props} from '@ngrx/store';
 import {User} from './model/user.model';
 
+/**
+ * We are going to call it login.
+ * And let's define here what looks like a new
+ * action by calling the Create Action Indirects method.
+ * So here we are going to specify a couple of arguments.
+ * Only the first one is mandatory, which is the action
+ * type now, as we did before here in the login
+ * component, we could define here a unique string,
+ * such as, for example, login action.
+ */
 
 export const login = createAction(
-    "[Login Page] User Login",
+  /**
+   * And let's define here what looks like a new action by calling
+   * the Create Action Indirects method.
+   * So here we are going to specify a couple of arguments.
+   * Only the first one is mandatory, which is the action type now,
+   * as we did before here in the login
+   * component, we could define here a unique string, such as,
+   * for example, login action.
+   * Now, instead of doing this, we are going to define this
+   * action type string by following a very specific
+   * convention.
+   * So first, at the beginning of the action type between
+   * square brackets, we are going to define here
+   * the source of the action in the application.
+   * So we are going to define the place in the application
+   * where the action is getting dispatched.
+   * So this means that in general, we should not be dispatching
+   * the same action in multiple different.
+   * Screens and components of our application, for example,
+   * this action should only be dispatched by the
+   * login page and by no other part of the application.
+   * So here between square brackets, we simply put login page,
+   * which is the source of this action.
+   * This is going to be very useful because later on we are going
+   * to have an action log and we would like
+   * to be able to identify just by looking at the action in our
+   * action log where that action is coming from.
+   */
+  "[Login Page] User Login",
+  /**
+   * In all cases, we want to define that the login action as a payload,
+   *  is a plain JavaScript object containing one single property name
+   * the user, which is of type user,which is the value returned here
+   * by the call to the login method.
+So here in our action definition, we are going to add here a second argument, which is a call to the
+utility function props from Engineering's.
+So this function doesn't take any arguments, but it does take here one generic parameter.
+So this generic parameter is the type of the payload associated to this action.
+So as we have seen, the payload should be a plain JavaScript object containing one single property
+and nothing more called user, which should be of type user
+*/
     props<{user: User}>()
 );
+
+/**
+ * Now, here is one important thing about this log in constant that we have just defined.
+This is actually not the type definition for the user login action instead.
+This is what is known as an action creator.
+So login is not the definition of a class.
+It's actually a function that we need to call in order to create a new login action.
+
+const newLoginAction = login({})
+*/
 
 
 
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -35,7 +35,75 @@ export class LoginComponent implements OnInit {
 
   login() {
 
+
+      const val = this.form.value;
+
+      this.auth.login(val.email, val.password)
+          .pipe(
+              tap(user => {
+                console.log(user);
+                /**
+                 * Now, what is the benefit of using this particular
+                 * API based on the dispatching of action objects when
+                 * compared to a more common API, such as, for example,
+                 * a grid like API, where we will have here at
+                 * the level of the store methods such as update,
+                 * create or delete?
+
+Well, this creates a level of indirection here by dispatching actions, we are not explicitly telling
+
+the store how to modify the data.
+
+We are either communicating to the store an event that we know that has happened here at the level of
+
+the component, or we are issuing an explicit command.
+
+But it's the store that is going to decide what to do with the action and how it's going to modify its
+
+internal state.
+
+The store might, for example, do a request to the server and fetch some extra information about the
+
+user and add it here to the user profile in order to build a more complete profile.
+
+The store might do several things with the action.
+
+This component, the login component that is dispatching the action, only knows about the content of
+
+the action, but it does not know what the store is going to do with the action.
+
+This level of interaction is important because this ensures that the login component is not going to
+
+get tightly coupled to other parts of the application.
+So it's important that the login component remains and aware of the course card list of the course dialog
+and of the course screens.
+These are completely separate components that also interact with the store by dispatching actions to
+it.
+
+                 */
+                // now the store is receiving user profile
+                this.store.dispatch(login({user}));
+                  this.router.navigateByUrl('/courses');
+              })
+          )
+          .subscribe(
+              noop,
+              () => alert('Login Failed')
+          );
+
   }
 
 }
 
+/**
+ * If we want to make sure that if multiple login actions are
+ * being created throughout the application
+ * at different places, that they all share the
+same type of action type and payload.
+So in order to ensure that we are going to
+adhere to our authentication module and new
+file, we are going to add here a file that
+is going to contain all our authentication actions.
+Let's define them here, a new file and we are
+going to call it of dot actions dots inside this file.
+ */
