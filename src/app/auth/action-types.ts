3e12657bebdcecd1ef9994c808902a31a12692f0
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-09-13 19:53:43 -0700

    update lecture 10

--- src/app/auth/action-types.ts
+++ src/app/auth/action-types.ts
@@ -1,5 +1,25 @@
-
+/**
+ * So we are going to go here to the authentication module
+ * and we are going to define here a new file that
+ * we are going to call simply action types dots.
+ * So inside this file, we are going to import everything that is
+ * defined here in the authentication actions
+file.
+*/
 
 import * as AuthActions from './auth.actions';
+export { AuthActions };
 
-export {AuthActions};
+/**
+ * So if I use here authentication actions locally, I'm going to have access here via auto completion
+to the login logout and any other action created that would be available inside the of vindication actions
+file.
+So I'm now going to take off actions and I'm going to re-export it here in this file.
+Let's just re-export of actions.
+And with this simple typescript trick, we now have a convenient way elsewhere on the application to
+access all the authentication actions.
+For example, let's say by some reason we would like to access all the actions here.
+We could do so by importing authentication actions and again, via auto completion.
+We have here a full list of authentication actions.
+So a very useful trick for grouping related actions together.
+ */
--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,74 +1,34 @@
 import {createAction, props} from '@ngrx/store';
 import {User} from './model/user.model';
 
-/**
- * We are going to call it login.
- * And let's define here what looks like a new
- * action by calling the Create Action Indirects method.
- * So here we are going to specify a couple of arguments.
- * Only the first one is mandatory, which is the action
- * type now, as we did before here in the login
- * component, we could define here a unique string,
- * such as, for example, login action.
- */
+
 
 export const login = createAction(
-  /**
-   * And let's define here what looks like a new action by calling
-   * the Create Action Indirects method.
-   * So here we are going to specify a couple of arguments.
-   * Only the first one is mandatory, which is the action type now,
-   * as we did before here in the login
-   * component, we could define here a unique string, such as,
-   * for example, login action.
-   * Now, instead of doing this, we are going to define this
-   * action type string by following a very specific
-   * convention.
-   * So first, at the beginning of the action type between
-   * square brackets, we are going to define here
-   * the source of the action in the application.
-   * So we are going to define the place in the application
-   * where the action is getting dispatched.
-   * So this means that in general, we should not be dispatching
-   * the same action in multiple different.
-   * Screens and components of our application, for example,
-   * this action should only be dispatched by the
-   * login page and by no other part of the application.
-   * So here between square brackets, we simply put login page,
-   * which is the source of this action.
-   * This is going to be very useful because later on we are going
-   * to have an action log and we would like
-   * to be able to identify just by looking at the action in our
-   * action log where that action is coming from.
-   */
   "[Login Page] User Login",
-  /**
-   * In all cases, we want to define that the login action as a payload,
-   *  is a plain JavaScript object containing one single property name
-   * the user, which is of type user,which is the value returned here
-   * by the call to the login method.
-So here in our action definition, we are going to add here a second argument, which is a call to the
-utility function props from Engineering's.
-So this function doesn't take any arguments, but it does take here one generic parameter.
-So this generic parameter is the type of the payload associated to this action.
-So as we have seen, the payload should be a plain JavaScript object containing one single property
-and nothing more called user, which should be of type user
-*/
     props<{user: User}>()
 );
-
-/**
- * Now, here is one important thing about this log in constant that we have just defined.
-This is actually not the type definition for the user login action instead.
-This is what is known as an action creator.
-So login is not the definition of a class.
-It's actually a function that we need to call in order to create a new login action.
-
-const newLoginAction = login({})
-*/
-
 
 
 export const logout = createAction(
   "[Top Menu] Logout"
 );
+
+/**
+ * So right now, let's go ahead and define here a new action
+ * creator, which is going to be called logout.
+So this is going to allow us to create new logout actions.
+Let's call here, create action and let's define here the action type.
+So again, between square brackets, we are going to define the source of this action.
+And again, it's very important that we don't reuse multiple actions in different parts of the application.
+It's important instead to create different actions for different screens and components in the application
+so that we can always easily track back the source using the information here inside the square brackets,
+part of the action type.
+So these logout action will only be issued here by the top menu and nothing more.
+And here on the second part, we are going to simply report what has happened.
+So the user has logged out.
+So this is another example of an action, which is actually an event.
+We are reporting something that a component knows that has happened locally.
+This is also an example of an action that does not need any payload.
+So we are not going to add here a props argument to create action.
+So we now have here two different actions that are related
+ */
